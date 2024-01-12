95e3c148c9c15a8ea9b600bf21369cf3e65eab32
Jens Joschinski <jens.joschinski@tum.de>
2024-01-12 11:06:02 +0100

    create library header
    created public API following PIMPL idiom.
    Does not work yet due to problems in cmakelist.txt and easlylogger

---
+++ src/plants.cpp
@@ -0,0 +1,51 @@
+/*------------------------------------------------------------------------------
+Copyright (C) 2023 - present Jens Joschinski
+
+This file is part of the ECOLOPES PLANT MODEL.
+
+ECOLOPES PLANT MODEL is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+ECOLOPES PLANT MODEL is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with ECOLOPES PLANT MODEL. 
+If not, see <https://www.gnu.org/licenses/>. */
+
+// --------------------------------------------------------------------------
+ /* ECOLOPES PLANT MODEL is derived, modified and extended from FATE, https://github.com/leca-dev/RFate.git/ 
+ * Copyright (C) 2021 Isabelle Boulangeat, Damien Georges, Maya Guéguen, Wilfried Thuiller 
+ * For contributions to this particular file, see section "Authors and contributors".*/
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+// Authors and contributors to this file:
+// Jens Joschinski
+// ----------------------------------------------------------------------------
+#include "p_PlantModel.h" //implementation
+#include "plants.h" //public API
+
+
+Plants::Plants(const std::string& inputFile, const std::vector<std::string>& keys){
+    pImpl = std::make_unique<PlantModel>(inputFile, keys);
+}
+
+Plants::~Plants() = default;
+
+void Plants::initialize(int years) {
+    pImpl->initialize(years);
+}
+
+void Plants::TPlusOne_JJ() {
+    pImpl->TPlusOne_JJ();
+}
+
+void Plants::createInputMaps(const Landscape<std::string>& soils, const Landscape<std::map<std::string, double>>& disturbance, bool init) {
+    pImpl->createInputMaps(soils, disturbance, init);
+}
+
+Landscape<double> Plants::getPFGabund(int PFG_id) {
+    return pImpl->getPFGabund(PFG_id);
+}
---
+++ src/plants.h
@@ -0,0 +1,49 @@
+/*------------------------------------------------------------------------------
+Copyright (C) 2023 - present Jens Joschinski
+
+This file is part of the ECOLOPES PLANT MODEL.
+
+ECOLOPES PLANT MODEL is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+ECOLOPES PLANT MODEL is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with ECOLOPES PLANT MODEL. 
+If not, see <https://www.gnu.org/licenses/>. */
+
+// --------------------------------------------------------------------------
+ /* ECOLOPES PLANT MODEL is derived, modified and extended from FATE, https://github.com/leca-dev/RFate.git/ 
+ * Copyright (C) 2021 Isabelle Boulangeat, Damien Georges, Maya Guéguen, Wilfried Thuiller 
+ * For contributions to this particular file, see section "Authors and contributors".*/
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+// Authors and contributors to this file:
+// Jens Joschinski
+// ----------------------------------------------------------------------------
+#ifndef PLANTS_H
+#define PLANTS_H
+
+#include <memory>
+#include "Landscape.h"
+
+class PlantModel;
+
+class Plants {
+public:
+	explicit Plants(const std::string& inputFile, const std::vector<std::string>& keys);
+    ~Plants();
+
+    void initialize(int years);
+    void TPlusOne_JJ();
+    void createInputMaps(const Landscape<std::string>& soils, const Landscape<std::map<std::string, double>>& disturbance, bool init);
+    Landscape<double> getPFGabund(int PFG_id);
+
+private:
+    std::unique_ptr<PlantModel> pImpl; // pointer to the implementation
+};
+
+#endif //PLANTS_H
